40e0eb46ab151ee74266545b967dae6ece4705f6
Divon2 <91172013+Divon2@users.noreply.github.com>
2021-11-17 09:47:55 +0700

    Add files via upload

---
+++ Algo Uprak 08 Desvon.py
@@ -0,0 +1,11 @@
+x = input ('Masukkan nama :') 
+print('Kamu menulis nama anda : ',x) 
+y = len (x) 
+print('jumlah hurufnya ', y) 
+print ('Huruf ganjilnya adalah :', x [1]) 
+print ('Huruf ganjilnya adalah :', x [3]) 
+print ('Huruf ganjilnya adalah :', x [5]) 
+ 
+
+
+
